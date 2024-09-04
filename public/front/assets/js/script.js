ed912e3f40e0bc1a23e9c5a068d480a787ba6b39
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-09-04 21:10:22 +0300

    hero slider section added

---
+++ public/front/assets/js/script.js
@@ -0,0 +1,19 @@
+// hero slider
+
+var swiper = new Swiper(".heroSlider", {
+    spaceBetween: 30,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
---
+++ resources/views/partials/hero.blade.php
@@ -0,0 +1,15 @@
+<section class="hero">
+    <div class="swiper heroSlider">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide"><img src="{{ asset('front/assets/images/slider1.webp') }}" alt="heroImage"></div>
+          <div class="swiper-slide"><img src="{{ asset('front/assets/images/slider2.webp') }}" alt="heroImage"></div>
+          <div class="swiper-slide"><img src="{{ asset('front/assets/images/slider3.webp') }}" alt="heroImage"></div>
+          <div class="swiper-slide"><img src="{{ asset('front/assets/images/slider4.webp') }}" alt="heroImage"></div>
+          <div class="swiper-slide"><img src="{{ asset('front/assets/images/slider5.webp') }}" alt="heroImage"></div>
+        </div>
+        <div class="swiper-pagination"></div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+      </div>
+
+</section>

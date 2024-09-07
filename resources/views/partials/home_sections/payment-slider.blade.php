77f3d426bcddc219b1011cc27b9c8d897b698a48
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-09-07 22:38:16 +0300

    payment section added

--- resources/views/partials/home_sections/payment-slider.blade.php
+++ resources/views/partials/home_sections/payment-slider.blade.php
@@ -8,8 +8,10 @@
                 <div class="swiper paymentSlider">
                     <div class="swiper-wrapper pickProductWrapper">
                         @foreach($paymentSlider as $item)
-                        <div class="swiper-slide">
+                        <div class="swiper-slide paymentSliderWrapper">
                             <img src="{{ $item['image'] }}" alt="productImage">
+                            <span class="pick-card-name">{{ $item['desc'] }}</span>
+                            <span class="link">Detayları İncele ></span>
                         </div>
                         @endforeach
                     </div>

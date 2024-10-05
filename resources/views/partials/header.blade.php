f7e108cb6803e1f49cf2b8b8a45c283c2f86d5a9
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-10-05 19:09:53 +0300

    responsive fix

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -13,113 +13,116 @@
             </div>
         </div>
     </div>
-    <div class="header-center">
-        <div class="header-container">
-            <div class="header-center-left">
-                <div class="input">
-                    <input type="text" placeholder="Arama">
-                    @include('partials.svg.search-svg')
+    <div class="header-wrapper">
+        <div class="header-center">
+            <div class="header-container">
+                <div class="header-center-left">
+                    <div class="input">
+                        <input type="text" placeholder="Arama">
+                        @include('partials.svg.search-svg')
+                    </div>
+                </div>
+                <div class="header-center-logo">
+                    <a href="#">
+                        @include('partials.svg.logo-svg')
+                    </a>
+                </div>
+                <div class="header-center-right">
+                    <ul class="header-center-list">
+                        <li>
+                            <a href="#">
+                                <img src="https://static.personaclick.com/assets/bell.svg" alt="zil">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Giriş Yap
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Favorilerim
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Sepetim
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
-            <div class="header-center-logo">
-                <a href="#">
-                    @include('partials.svg.logo-svg')
-                </a>
-            </div>
-            <div class="header-center-right">
-                <ul class="header-center-list">
+        </div>
+        <div class="header-bottom">
+            <div class="header-container">
+                <ul class="header-bottom-list">
                     <li>
                         <a href="#">
-                            <img src="https://static.personaclick.com/assets/bell.svg" alt="zil">
+                            <img src="https://cdn.vivense.com/images/icon/vvns-collection-menu-icon.svg"
+                                alt="collection">
+                            Vivense Collection
                         </a>
                     </li>
                     <li>
                         <a href="#">
-                            Giriş Yap
+                            <img src="https://cdn.vivense.com/images/icon/vvns-luna-menu-icon.svg" alt="luna">
+                            Luna Beeding
                         </a>
                     </li>
                     <li>
                         <a href="#">
-                            Favorilerim
+                            <img src="https://img.vivense.com/images/15a215179d6e4a06932559e45edd00eb.svg"
+                                alt="vivenseIcon">
+                            EN İYİ FİYAT
                         </a>
                     </li>
                     <li>
                         <a href="#">
-                            Sepetim
+                            OTURMA ODASI
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            YATAK ODASI
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            YEMEK ODASI + MUTFAK
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            BAHÇE
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            DOLAP + ANTRE
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            ÇALIŞMA ODASI
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            ÇOCUK + GENÇ
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            DEKORASYON
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            AYDINLATMA
                         </a>
                     </li>
                 </ul>
             </div>
-        </div>
-    </div>
-    <div class="header-bottom">
-        <div class="header-container">
-            <ul class="header-bottom-list">
-                <li>
-                    <a href="#">
-                        <img src="https://cdn.vivense.com/images/icon/vvns-collection-menu-icon.svg" alt="collection">
-                        Vivense Collection
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <img src="https://cdn.vivense.com/images/icon/vvns-luna-menu-icon.svg" alt="luna">
-                        Luna Beeding
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <img src="https://img.vivense.com/images/15a215179d6e4a06932559e45edd00eb.svg"
-                            alt="vivenseIcon">
-                        EN İYİ FİYAT
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        OTURMA ODASI
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        YATAK ODASI
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        YEMEK ODASI + MUTFAK
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        BAHÇE
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        DOLAP + ANTRE
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        ÇALIŞMA ODASI
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        ÇOCUK + GENÇ
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        DEKORASYON
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        AYDINLATMA
-                    </a>
-                </li>
-            </ul>
         </div>
     </div>
     @include('partials.banner')
--- resources/views/partials/home_sections/flash-product.blade.php
+++ resources/views/partials/home_sections/flash-product.blade.php
@@ -2,35 +2,35 @@
     $flashPrices = [
         [
             'image' => asset('front/assets/images/stock1.webp'),
-            'price' => 26.499
+            'price' => 26.499,
         ],
         [
             'image' => asset('front/assets/images/stock2.webp'),
-            'price' => 32.999
+            'price' => 32.999,
         ],
         [
             'image' => asset('front/assets/images/stock3.webp'),
-            'price' => 5.199
+            'price' => 5.199,
         ],
         [
             'image' => asset('front/assets/images/stock4.webp'),
-            'price' => 11.199
+            'price' => 11.199,
         ],
         [
             'image' => asset('front/assets/images/stock5.webp'),
-            'price' => 2.499
+            'price' => 2.499,
         ],
         [
             'image' => asset('front/assets/images/stock6.webp'),
-            'price' => 3.999
+            'price' => 3.999,
         ],
         [
             'image' => asset('front/assets/images/stock7.webp'),
-            'price' => 6.999
+            'price' => 6.999,
         ],
         [
             'image' => asset('front/assets/images/stock8.webp'),
-            'price' => null
+            'price' => null,
         ],
     ];
 @endphp
@@ -43,15 +43,15 @@
             </span>
         </div>
         <div class="row">
-            @foreach($flashPrices as $item)
-            <a href="#" class="col-lg-3 ">
-                <div class="flash-card">
-                <img class="img-field" src="{{ $item['image'] }}" alt="Flash Image" loading="lazy">
-                @if ($item['price'] !== null)
-             <span class="price">{{ $item['price'] }} TL</span>
-                @endif
-            </div>
-            </a>
-        @endforeach
-    </div>
+            @foreach ($flashPrices as $item)
+                <a href="#" class="col-lg-3 col-6">
+                    <div class="flash-card">
+                        <img class="img-field" src="{{ $item['image'] }}" alt="Flash Image" loading="lazy">
+                        @if ($item['price'] !== null)
+                            <span class="price">{{ $item['price'] }} TL</span>
+                        @endif
+                    </div>
+                </a>
+            @endforeach
+        </div>
 </section>
